24d445b88e8b499a4deca7d4c36f50ec07321d1c
sushant naik <sushantmnaik08@gmail.com>
2026-05-20 11:15:44 +0530

    Ap

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///usr.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 dbs = SQLAlchemy(app)
-
+dbs.init_app(app)
 from usr import usr
 
 class dbsq(dbs.Model):
